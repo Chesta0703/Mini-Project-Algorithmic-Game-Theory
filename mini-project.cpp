3a7f1bf0473b5e4bf7b0aa31891b7ef7587c2444
Tushar Kanwaria <72695288+tusharkanwaria@users.noreply.github.com>
2022-11-04 20:40:47 +0530

    Add files via upload

--- mini-project.cpp
+++ mini-project.cpp
@@ -41,6 +41,9 @@ const ld EPS = 1e-9;
 
 void solve(float p11, float p22, float p33) {
 
+   
+
+
 //calculating survival when two players left 
 
 float arr[6][3] = {0};
@@ -52,6 +55,13 @@ float p3 = p33/100;
 
 // cout<<endl<<p1<<endl<<p2<<endl<<p3<<endl;
 
+  float p4=0;
+
+     if(p3>p2)
+     { p4 = p2;
+       p2=p3;
+       p3=p4;
+     }
 
 
 //arr[][0] = survival chance A ,  arr[][1] : survival chance B , arr[][2]:C
@@ -129,33 +139,125 @@ float p3 = p33/100;
 
 
 
+//====================================================================
+
+double thir[6][4] = {0};
+double four[6][3] = {0};
+
+// survival chance when shooting at A, B, C, When miss deliberately
+
+// ABC
+
+//p(A)-----------------------------------------
+thir[0][0] = 0;
+
+
+//P(A hits) × P(A,CA) + P(A misses) × P(A,BCA)-----------------
+thir[0][1] = p1*(arr[4][0])+(1-p1)*((1-p2));
+
+
+//shooting C =>P(A hits) × P(A,BA) + P(A misses) × P(A,BCA)-------
+thir[0][2] = p1*( arr[2][0]) +(1-p1);
+
    
+//Survival chance when missing deliberately:
 
 
+//So, A shoots B, which means that
+//P(A,ABC) = 1/2,
+//P(B,ABC) = 0, and
+//P(C,ABC) = P(C,CA) = 1/2
+four[0][0] = 1-p3 ;
+four[0][1] = 0 ;
+four[0][2] =  arr[4][2];
 
 
+//So, A shoots B, which means that
+//P(A,ACB) = 1/2,
+//P(B,ACB) = 0, and
+//P(C,ACB) = P(C,CA) = 1/2
+four[1][0] = 1-p3 ;
+four[1][1] = 0 ;
+four[1][2] =  arr[4][2];
+
+//Therefore, B shoots A, which means that
+//P(A,BAC) = P(B misses) × P(A,ACB) = 1/10, 
+//P(B,BAC) = 16/45,
+//P(C,BAC) = P(B hits) × P(C,CB) + P(B misses) × P(C,ACB) = 49/90
+four[2][0] = (1-p2)*four[1][0];
+four[2][1] = p2*arr[5][1];
+four[2][2] = p2*arr[5][2] + (1-p2)*four[1][2];
 
 
+//So, C should miss deliberately (fire "into the air"), which means that
+//P(A,CAB) = P(A,ABC) = 1/2, and
+//P(B,CAB) = P(B,ABC) = 0
+//P(C,CAB) = 1/2,
+thir[3][0] = p3*arr[3][2] + (1-p3)*four[0][2];
+
+four[3][0] = four[0][0];
+four[3][1] = 0;
+four[3][2] = 1-p3;
 
 
+//	Therefore, B shoots A, which means that
+//P(A,BCA) = P(B misses) × P(A,CAB) = 1/10, and
+//P(B,BCA) = 16/45,
+//P(C,BCA) = P(B hits) × P(C,CB) + P(B misses) × P(C,CAB) = 49/90
+
+four[4][0] = (1-p2)*four[3][0];
+four[4][1] = four[2][1] = p2*arr[5][1];
+four[4][2] = p2* arr[5][2] + (1-p2)*four[3][2];
 
 
-
-
-
-
-
-
-
+//So, C should miss deliberately (fire "into the air"), which means that
+//P(A,CBA) = P(A,BAC) = 1/10, and
+//P(B,CBA) = P(B,BAC) = 16/45
+//P(C,CBA) = 49/90,
+four[5][0] = (1-p2)*four[1][0];
+four[5][1] = four[2][1];
+four[5][2] = four[2][2];
 
 
 
 
    for (int i = 0; i < 6; i++)
    {
-       cout<<arr[i][0]<<" "<<arr[i][1]<<" "<<arr[i][2]<<endl;
+      // cout<<arr[i][0]<<" "<<arr[i][1]<<" "<<arr[i][2]<<endl;
+   }
+
+cout<<endl;
+cout<<endl;
+     for (int i = 0; i < 6; i++)
+   {
+      // cout<<four[i][0]<<" "<<four[i][1]<<" "<<four[i][2]<<endl;
    }
    
+
+float s1=0;
+float s2=0;
+float s3=0;
+
+  for (int i = 0; i < 6; i++)
+  {
+      s1 = s1 + four[i][0];
+      s2 = s2 + four[i][1];
+      s3 = s3 + four[i][2];
+  } 
+
+
+    cout<<"Survival rate of "<<endl;
+    cout<<"A : "<<s1/6<<endl;
+    if(p4==0){
+    cout<<"B : "<<s2/6<<endl;
+    cout<<"C : "<<s3/6<<endl;}
+    else
+     {
+    cout<<"B : "<<s3/6<<endl;
+    cout<<"C : "<<s2/6<<endl;}
+
+
+
 }
 
 
@@ -176,8 +278,12 @@ int main() {
       cout<<"Player 3: ";
      cin>>p3;
 
+    
+
+
 cout<< endl;
-     solve(p1,p2,p3);
+solve(p1,p2,p3);
+
 
 
 
